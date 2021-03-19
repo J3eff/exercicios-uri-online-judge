fa7774de79292ec8230674ad5c9006dce74ab938
J3eff <jeffersonvideo125@gmail.com>
2021-03-19 07:25:20 -0300

    uri1004 - Produto simples

---
+++ uri1004/uri1004/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uri1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            int PROD = x * y;
+
+            Console.WriteLine("PROD = " + PROD);
+        }
+    }
+}
