b36a5d168678b15b5dd2f7b0065df74d6ed1b4d2
J3eff <jeffersonvideo125@gmail.com>
2021-03-19 10:07:18 -0300

    uri1005 - Media 1

---
+++ uri1005/uri1005/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double MEDIA = ((3.5 * nota1) + 7.5 * nota2) / 11;
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
