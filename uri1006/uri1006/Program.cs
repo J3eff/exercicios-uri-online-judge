7981042a99093cfde6b2ce43d107b946f90e5fcb
J3eff <jeffersonvideo125@gmail.com>
2021-03-25 09:00:25 -0300

    uri1006 - Média 2

---
+++ uri1006/uri1006/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (((2.0 * a) + 3.0 * b) + 5.0 * c) / 10.0;
+
+            Console.WriteLine("MEDIA = {0}", media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
