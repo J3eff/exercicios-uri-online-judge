f6fa7c13b20f06d5c758cbe4f2b9bd33d5a5e592
J3eff <jeffersonvideo125@gmail.com>
2021-03-25 23:30:14 -0300

    uri1008 - Salario

---
+++ uri1008/uri1008/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //link da atividade: 
+            //https://www.urionlinejudge.com.br/judge/pt/problems/view/1008 
+
+            int number = int.Parse(Console.ReadLine());
+            int hrsTrabalhada = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NUMBER = {0}", number);
+            double sumSalary = hrsTrabalhada * salary;
+
+            Console.WriteLine("SALARY = U$ {0}", sumSalary.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
