b57b355e4129c292ec8e9e320715412288d2541a
J3eff <jeffersonvideo125@gmail.com>
2021-03-25 23:51:05 -0300

    uri1009 - ]Salario com Bônus

---
+++ uri1009/uri1009/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercico: https://www.urionlinejudge.com.br/judge/pt/problems/view/1009
+
+            string name = Console.ReadLine();
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double commission = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = salary + ((commission * 15) / 100);
+
+            Console.WriteLine("TOTAL = R$ {0}", total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
