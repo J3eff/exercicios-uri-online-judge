1d6c475fb02e075f2ac549cab9e2e87966a2a1e4
J3eff <jeffersonvideo125@gmail.com>
2021-04-07 22:31:12 -0300

    uri1011 e uri1012

---
+++ uri1011/uri1011/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Link do exercicio: https://www.urionlinejudge.com.br/judge/pt/problems/view/1011
+
+            double pi = 3.14159;
+            int r = int.Parse(Console.ReadLine());
+
+            double volume = 4.0 / 3 * pi * Math.Pow(r, 3);
+
+            Console.WriteLine("VOLUME = {0}", volume.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ uri1012/uri1012/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Link do exercicio: https://www.urionlinejudge.com.br/judge/pt/problems/view/1012
+
+            string[] vet = Console.ReadLine().Split(' ');
+            double a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            double triangulo = a * c / 2;
+            double circulo = 3.14159 * Math.Pow(c, 2);
+            double trapezio = (a + b) * c / 2;
+            double quadrado = b * b;
+            double retangulo = a * b;
+
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.CurrentUICulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.CurrentUICulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.CurrentUICulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.CurrentUICulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.CurrentUICulture));
+
+        }
+    }
+}
