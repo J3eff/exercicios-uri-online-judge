475aeb299ae62859fcd68f3c8800a01c8d13260a
J3eff <jeffersonvideo125@gmail.com>
2021-04-19 18:01:25 -0300

    uri1016 - distancia

---
+++ uri1016/uri1016/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uri1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanciaKm = int.Parse(Console.ReadLine());
+
+            int tempo = 2 * distanciaKm;
+
+            Console.WriteLine($"{tempo} minutos");
+        }
+    }
+}
