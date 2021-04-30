d08dadf6c945bd19acf4630e37e0fba18a044bfa
J3eff <jeffersonvideo125@gmail.com>
2021-04-29 22:27:32 -0300

    uri1019 - Conversao de tempo

---
+++ uri1019/uri1019/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //link atividade: https://www.urionlinejudge.com.br/judge/pt/runs/code/22604051
+            int n = int.Parse(Console.ReadLine());
+
+            int hora = n / 3600;
+            int minuto = (n % 3600) / 60;
+            int segundo = (n % 3600) % 60;
+            Console.WriteLine(hora + ":" + minuto + ":" + segundo);
+        }
+    }
+}
