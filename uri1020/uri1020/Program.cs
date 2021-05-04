6fb9b0eefbddb557cd6f7ce8a9e9f9383fafb936
J3eff <jeffersonvideo125@gmail.com>
2021-05-04 20:54:47 -0300

    uri1020 - Idade em Dias

---
+++ uri1020/uri1020/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uri1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //link atividade: https://www.urionlinejudge.com.br/judge/pt/runs/code/22604505
+            int idade = int.Parse(Console.ReadLine());
+
+            int ano = idade / 365;
+            int mes = (idade % 365) / 30;
+            int dia = (idade % 365) % 30;
+
+            Console.WriteLine($"{ano} ano(s)"); 
+            Console.WriteLine($"{mes} mes(es)"); 
+            Console.WriteLine($"{dia} dia(s)"); 
+        }
+    }
+}
