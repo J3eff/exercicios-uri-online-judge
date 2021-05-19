ba9acf6b94d05cfe7ffb9417b8bdd9cb6c086829
J3eff <jeffersonvideo125@gmail.com>
2021-05-19 08:19:06 -0300

    uri1038 - Lanche

---
+++ uri1038/uri1038/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            int cod = int.Parse(vet[0]);
+            int qtd = int.Parse(vet[1]);
+
+            double total = 0;
+
+            if (cod == 1)
+            {
+                total = qtd * 4.00;
+            }
+            else if (cod == 2)
+            {
+                total = qtd * 4.50;
+            }
+            else if (cod == 3)
+            {
+                total = qtd * 5.00;
+            }
+            else if (cod == 4)
+            {
+                total = qtd * 2.00;
+            }
+            else if (cod == 5)
+            {
+                total = qtd * 1.50;
+            }
+
+            Console.WriteLine($"Total: R$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
