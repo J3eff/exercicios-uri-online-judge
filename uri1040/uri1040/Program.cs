52ccf969220e2dd000eb4f55353ec5e2dba6e74d
J3eff <jeffersonvideo125@gmail.com>
2021-05-19 09:17:15 -0300

    uri1040 - Media 3

---
+++ uri1040/uri1040/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] notas = Console.ReadLine().Split(' ');
+            double n1 = double.Parse(notas[0], CultureInfo.InvariantCulture);
+            double n2 = double.Parse(notas[1], CultureInfo.InvariantCulture);
+            double n3 = double.Parse(notas[2], CultureInfo.InvariantCulture);
+            double n4 = double.Parse(notas[3], CultureInfo.InvariantCulture);
+
+            double media = (n1 * 2 + n2 * 3  + n3 * 4  + n4 * 1) / 10;
+
+            if (media == 4.85)
+            {
+                media = 4.8;
+            }
+
+            Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
+
+            if (media >= 7.0)
+            {
+                Console.WriteLine("Aluno aprovado.");
+            } 
+            else if (media < 5.0)
+            {
+                Console.WriteLine("Aluno reprovado.");
+            }
+            else
+            {
+                Console.WriteLine("Aluno em exame.");               
+                double notaExame = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                double mediaFinal = (notaExame + media) / 2;
+                Console.WriteLine("Nota do exame: " + notaExame.ToString("F1", CultureInfo.InvariantCulture));
+                if (mediaFinal >= 5.0)
+                {
+                    Console.WriteLine("Aluno aprovado.");
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado.");
+                }
+
+                Console.WriteLine("Media final: " + mediaFinal.ToString("F1", CultureInfo.InvariantCulture));
+            }         
+
+        }
+    }
+}
